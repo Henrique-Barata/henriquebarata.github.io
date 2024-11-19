2e59d5d959f47647c12046672fdf51a56dd19df0
Henrique-Barata <148144428+Henrique-Barata@users.noreply.github.com>
2024-11-19 11:14:19 +0000

    again

--- js/index.js
+++
@@ -1,5 +0,0 @@
-function openImage(imgElement) {
-    const imgSrc = imgElement.src;
-    const newWindow = window.open("", "_blank");
-    newWindow.document.write(`<img src="${imgSrc}" style="width:100%">`);
-  }
---
+++ main.js
@@ -0,0 +1,5 @@
+import bay from "./bay.min.mjs";
+import modal from "./modal.mjs";
+
+bay();
+bay.create('bay-modal', modal, ['open']);
