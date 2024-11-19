54a1df3173dd80f9000340d9dca09faf3f4956a6
Henrique-Barata <148144428+Henrique-Barata@users.noreply.github.com>
2024-11-19 13:50:46 +0000

    kkk

---
+++ js/jsIndex.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const sections = document.querySelectorAll('section');
+
+  const options = {
+      threshold: 0.1
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.style.opacity = 1;
+              entry.target.style.transform = 'translateY(0)';
+          } else {
+              entry.target.style.opacity = 0;
+              entry.target.style.transform = 'translateY(50px)';
+          }
+      });
+  }, options);
+
+  sections.forEach(section => {
+      observer.observe(section);
+  });
+
+  document.querySelectorAll('nav a').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+              behavior: 'smooth'
+          });
+      });
+  });
+});
